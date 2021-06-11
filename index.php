0a36be47703a71a9625d86bb2eead66c5cae4a98
Vanessa-D <vanessa.ducloux@laposte.net>
2021-06-11 18:36:27 +0200

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title>Exercice 1</title>
+</head>
+<body>
+    <p>
+        Créer une variable, lui affecter une chaîne de caractères et l'afficher : 
+    </p>
+    <div class="col-3 mx-auto mt-5 text-center" style="border: burlywood 2px solid;">
+        <?php
+            $string = "Tout va bien ?"; /* Déclaration d'une variable */
+            echo $string; /* Affichage d'une variable */
+        ?>
+    </div>
+
+</body>
+</html>
